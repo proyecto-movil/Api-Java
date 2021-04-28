040f2a9ac50a9b08e788f7ea91b2154490e47604
XantX <hp_xantx@hotmail.com>
2021-04-27 21:01:47 -0500

    Add folder structure and init proyect

---
+++ src/main/java/com/example/ilenguageapi/ILenguageApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ilenguageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILenguageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ILenguageApiApplication.class, args);
+    }
+
+}
