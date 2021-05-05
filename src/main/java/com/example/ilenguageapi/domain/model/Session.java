8747df65d64c3666090c9c815ed331790c31b873
LuisMiguelEstradaFernandez <61571741+LuisMiguelEstradaFernandez@users.noreply.github.com>
2021-05-05 18:41:38 -0500

    feat: session model first version session model

---
+++ src/main/java/com/example/ilenguageapi/domain/model/Session.java
@@ -0,0 +1,10 @@
+package com.example.ilenguageapi.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="sessions")
+public class Session extends AuditModel {
+
+}
