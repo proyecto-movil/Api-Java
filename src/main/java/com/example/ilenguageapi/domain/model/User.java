355471881597c0126fd77c98625a5ad6069dba3b
XantX <hp_xantx@hotmail.com>
2021-05-05 16:42:37 -0500

    feat: UserModel init add initial data for user model TODO: add relashionship with role an interest

---
+++ src/main/java/com/example/ilenguageapi/domain/model/User.java
@@ -0,0 +1,111 @@
+package com.example.ilenguageapi.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "Users")
+public class User extends AuditModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 50)
+    private String name;
+
+    @NotNull
+    @Size(max = 50)
+    private String lastName;
+
+    @NotNull
+    @Column(unique = true)
+    private String Email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    @Lob
+    @Size(max = 245)
+    private String description;
+
+
+    //TODO: delete contructor on subscription
+    @NotNull
+    private Subscription subscriptionActive;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST,CascadeType.MERGE},
+    mappedBy = "users")
+    private List<Subscription> subscriptions;
+
+    //TODO:Implement Role
+/*
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "role_id",nullable = false)
+    JsonIgnore
+    private Role role;
+*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Subscription> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public void setSubscriptions(List<Subscription> subscriptions) {
+        this.subscriptions = subscriptions;
+    }
+}
