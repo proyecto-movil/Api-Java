f496aa73866274502b664f220be4995f93684529
XantX <hp_xantx@hotmail.com>
2021-05-06 18:44:10 -0500

    feat: UserResource userResource data TODO: saveuserResource

--- src/main/java/com/example/ilenguageapi/resource/UserResource.java
+++ src/main/java/com/example/ilenguageapi/resource/UserResource.java
@@ -1,4 +1,69 @@
 package com.example.ilenguageapi.resource;
 
-public class UserResource {
+import com.example.ilenguageapi.domain.model.AuditModel;
+
+public class UserResource extends AuditModel {
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private String description;
+    private String profilePhoto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
 }
