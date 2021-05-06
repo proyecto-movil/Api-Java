e15df92a4de6ac7946af3cc7b1ee07eee74ffeff
Sebastian <sebaas14toulier@gmail.com>
2021-05-06 01:53:01 -0500

    refactor: getters and setters on save subscription resource

--- src/main/java/com/example/ilenguageapi/resource/SaveSubscriptionResource.java
+++ src/main/java/com/example/ilenguageapi/resource/SaveSubscriptionResource.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.Positive;
 
 public class SaveSubscriptionResource {
     @Positive(message ="Price must have a positive value")
-    private float Price;
+    private float price;
 
     @NotNull
     @Positive(message ="Price must have a positive value")
@@ -15,4 +15,30 @@ public class SaveSubscriptionResource {
     @Size(min = 3, max = 20, message="Name must have between 3 and 20 characters")
     private String name;
 
+    public float getPrice() {
+        return price;
+    }
+
+    public int getMonthDuration() {
+        return monthDuration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public SaveSubscriptionResource setPrice(float price) {
+        this.price = price;
+        return this;
+    }
+
+    public SaveSubscriptionResource setMonthDuration(int monthDuration) {
+        this.monthDuration = monthDuration;
+        return this;
+    }
+
+    public SaveSubscriptionResource setName(String name) {
+        this.name = name;
+        return this;
+    }
 }
