779fccebb67de81525bf652aa0630df0e3b577db
Sebastian <sebaas14toulier@gmail.com>
2021-05-06 10:42:06 -0500

    feat: subscription service is implemented

--- src/main/java/com/example/ilenguageapi/service/SubscriptionServiceImpl.java
+++ src/main/java/com/example/ilenguageapi/service/SubscriptionServiceImpl.java
@@ -1,44 +1,61 @@
 package com.example.ilenguageapi.service;
 
 import com.example.ilenguageapi.domain.model.Subscription;
+import com.example.ilenguageapi.domain.repository.SubscriptionRepository;
 import com.example.ilenguageapi.domain.service.SubscriptionService;
+import com.example.ilenguageapi.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Optional;
+
 public class SubscriptionServiceImpl  implements SubscriptionService {
 
-
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
 
 
     @Override
     public Page<Subscription> getAllSubscriptions(Pageable pageable) {
-        return null;
+        return subscriptionRepository.findAll(pageable);
     }
 
     @Override
     public Subscription createSubscription(Subscription subscription) {
-        return null;
+        return subscriptionRepository.save(subscription);
     }
 
     @Override
     public Subscription updateSubscription(int subscriptionId, Subscription subscriptions) {
-        return null;
+        //? would this work ok?
+        Subscription subscription = subscriptionRepository.findById(subscriptionId)
+                .orElseThrow(()->new ResourceNotFoundException("Subscription", "Id", subscriptionId));
+        subscription.setPrice(subscriptions.getPrice());
+        subscription.setName(subscriptions.getName());
+        subscription.setMonthDuration(subscriptions.getMonthDuration());
+        return subscriptionRepository.save(subscription);
     }
 
     @Override
     public ResponseEntity<?> deleteSubscription(int subscriptionId) {
-        return null;
+        Subscription subscription = subscriptionRepository.findById(subscriptionId)
+                .orElseThrow(()-> new ResourceNotFoundException("Subscription", "Id", subscriptionId));
+        subscriptionRepository.delete(subscription);
+        return ResponseEntity.ok().build();
     }
 
     @Override
     public Subscription getBySubscriptionId(int subscriptionId) {
-        return null;
+        return subscriptionRepository.findById(subscriptionId)
+                .orElseThrow(()->new ResourceNotFoundException("Subscription", "Id", subscriptionId));
     }
 
     @Override
     public Subscription getByName(String name) {
-        return null;
+        return subscriptionRepository.findByName(name)
+                .orElseThrow(()-> new ResourceNotFoundException("Subscription", "Name", name));
     }
 
     @Override
