77b8b51efea2065320e27962e0c7897f6481a74f
Sebastian Trujillo <ippo_sebas@hotmail.com>
2021-05-05 21:30:34 -0500

    Add Entity Role

---
+++ src/main/java/com/example/ilenguageapi/domain/model/Role.java
@@ -0,0 +1,45 @@
+package com.example.ilenguageapi.domain.model;
+
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="Roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max=20)
+    @NaturalId
+    public String name;
+
+    public Role() {
+    }
+
+    public Role( @NotNull String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
