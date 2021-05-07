e1ab676886c0c43dea2bb3950438a2fc3be01dfe
LuisMiguelEstradaFernandez <61571741+LuisMiguelEstradaFernandez@users.noreply.github.com>
2021-05-07 16:19:34 -0500

    feat: add sessionController

--- src/main/java/com/example/ilenguageapi/service/SessionServiceImpl.java
+++ src/main/java/com/example/ilenguageapi/service/SessionServiceImpl.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SessionServiceImpl implements SessionService {
 
     @Autowired
