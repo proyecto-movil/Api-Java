8150ab7b6ee583d1b03676a6502a100c178acb46
Sebastian <sebaas14toulier@gmail.com>
2021-05-07 02:34:39 -0500

    refactore: add the necesary annotations to documment the subscription

--- src/main/java/com/example/ilenguageapi/controller/SubscriptionsController.java
+++ src/main/java/com/example/ilenguageapi/controller/SubscriptionsController.java
@@ -5,6 +5,10 @@ import com.example.ilenguageapi.domain.model.Subscription;
 import com.example.ilenguageapi.domain.service.SubscriptionService;
 import com.example.ilenguageapi.resource.SaveSubscriptionResource;
 import com.example.ilenguageapi.resource.SubscriptionResource;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -32,6 +36,10 @@ public class SubscriptionsController {
         return mapper.map(entity, SubscriptionResource.class);
     }
 
+    @Operation(summary="Get all", description="Get all the subscriptions", tags = {"subscriptions"} )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description="All subscriptions returned", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/subscriptions")
     public Page<SubscriptionResource> getAllSubscriptions(Pageable pageable){
         List<SubscriptionResource> subscriptions = subscriptionService.getAllSubscriptions(pageable)
@@ -41,36 +49,64 @@ public class SubscriptionsController {
         return new PageImpl<>(subscriptions, pageable, subscriptionCount);
     }
 
+    @Operation(summary="Save suscription", description="Save suscription", tags = {"subscriptions"} )
     @PostMapping("/subscriptions")
     public SubscriptionResource createSubscription(@Valid @RequestBody SaveSubscriptionResource resource){
         return convertToResource(subscriptionService.createSubscription(convertToEntity(resource)));
     }
 
+    @Operation(summary="Update suscription", description="Update suscription", tags = {"subscriptions"} )
     @PutMapping ("subscriptions/{id}")
     public SubscriptionResource updateSubscription(@PathVariable(name="id") int subscriptionId, @Valid @RequestBody SaveSubscriptionResource resource){
         return convertToResource(subscriptionService.updateSubscription(subscriptionId,convertToEntity(resource)));
     }
 
+    @Operation(summary="Delete suscription", description="Delete suscription", tags = {"subscriptions"} )
     @DeleteMapping("subscriptions/{id}")
     public ResponseEntity<?> deleteSubscriptions(@PathVariable(name="id") int subscriptionId){
         return subscriptionService.deleteSubscription(subscriptionId);
     }
 
+
+    @Operation(summary="Get by id", description="Get the subscription giving an id", tags = {"subscriptions"} )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description="All subscriptions returned", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description="Subscription Not Found", content = @Content(mediaType = "application/json"))
+
+    })
     @GetMapping("/subscriptions/{id}")
     public SubscriptionResource getSubscriptionById(@PathVariable(name="id") int subscriptionId){
         return convertToResource(subscriptionService.getBySubscriptionId(subscriptionId));
     }
 
+    @Operation(summary="Get by name", description="Get the subscription giving a name", tags = {"subscriptions"} )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description="All subscriptions returned", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description="Subscription Not Found", content = @Content(mediaType = "application/json"))
+
+    })
     @GetMapping("/subscriptions/{name}")
     public SubscriptionResource getSubscriptionByPrice(@PathVariable(name="name") String subscriptionName){
         return convertToResource(subscriptionService.getByName(subscriptionName));
     }
 
+    @Operation(summary="Get by price", description="Get the subscription giving the price", tags = {"subscriptions"} )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description="All subscriptions returned", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description="Subscription Not Found", content = @Content(mediaType = "application/json"))
+
+    })
     @GetMapping("/subscriptions/{price}")
     public SubscriptionResource getSubscriptionByPrice(@PathVariable(name="price") float producedPrice){
         return convertToResource(subscriptionService.getByPrice(producedPrice));
     }
 
+    @Operation(summary="Get by month duration", description="Get the subscription giving the month duration ", tags = {"subscriptions"} )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description="All subscriptions returned", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description="Subscription Not Found", content = @Content(mediaType = "application/json"))
+
+    })
     @GetMapping("/subscriptions/{monthDuration}")
     public SubscriptionResource getSubscriptionByMonthDuration(@PathVariable(name="monthDuration") int subscriptionMonthDuration){
     return convertToResource(subscriptionService.getByDuration(subscriptionMonthDuration));
