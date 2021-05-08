3eb5f48aed8b227cb4e4f5e50e3ef7e89ae86d77
XantX <hp_xantx@hotmail.com>
2021-05-08 06:42:27 -0500

    feat: add test for all crud add testing for all crud methods for userService

--- src/test/java/com/example/ilenguageapi/UserServiceImplTest.java
+++ src/test/java/com/example/ilenguageapi/UserServiceImplTest.java
@@ -12,8 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import javax.swing.text.html.Option;
 import java.util.Optional;
@@ -91,4 +94,56 @@ public class UserServiceImplTest {
                 .isInstanceOf(ResourceNotFoundException.class)
                 .hasMessage(expectedMessage);
     }
+
+    @Test
+    @DisplayName("When createdUserByUser with valid User Then Returns User")
+    public void whenCreatedUserByUserWithValidUserThenReturnsUser() {
+        //Arrange
+        User user = new User();
+        when(userRepository.save(user)).thenReturn(user);
+        //Act
+        User foundUser = userService.createUser(user);
+        //Assert
+
+        assertThat(user)
+                .isEqualTo(foundUser);
+    }
+
+    @Test
+    @DisplayName("When UpdateUserById with valid Id Then Return UpdatedUser")
+    public void whenUpdateUserByIdWithValidIdThenReturnUpdatedUser() {
+        //Arrage
+        Long userId = 15L;
+        User user = new User().setId(userId).setName("pepe").setLastName("palotes");
+        User newUser = new User().setId(userId).setName("Luis").setLastName("palotes");
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(userRepository.save(newUser)).thenReturn(newUser);
+        //Act
+        User foundUser = userService.updateUser(userId,newUser);
+        //Assert
+        assertThat(user).isNotEqualTo(foundUser);
+    }
+
+    @Test
+    @DisplayName("When UpdateUserById with Invalid Id Then Return UpdatedUser")
+    public void whenUpdateUserByIdWithInvalidIdThenReturnUpdatedUser() {
+        //Arrage
+        Long userId = 15L;
+        User userDetails = new User();
+        String template = "Resource %s not found for %s with value %s";
+        when(userRepository.findById(userId))
+                .thenReturn(Optional.empty());
+        String expectedMessage = String.format(template, "User", "id", userId);
+        //Act
+
+        Throwable exception = catchThrowable(() -> {
+            User updatedUser = userService.updateUser(userId, userDetails);
+        });
+        //Assert
+        assertThat(exception)
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage(expectedMessage);
+    }
 }
+
