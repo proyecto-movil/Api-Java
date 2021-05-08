8c402f29457e76cc45f33ee48cf2676d020fc478
XantX <hp_xantx@hotmail.com>
2021-05-08 07:15:42 -0500

    fix: add anotations for shedule model

--- src/main/java/com/example/ilenguageapi/domain/model/Schedule.java
+++ src/main/java/com/example/ilenguageapi/domain/model/Schedule.java
@@ -1,21 +1,36 @@
 package com.example.ilenguageapi.domain.model;
 
-public class Schedule {
-    private  String name;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "schedules")
+public class Schedule extends AuditModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotNull
+    private  String name;
+    @NotNull
     public int hoursDuration;
-    public Schedule(){
-
-    }
-    public Schedule( int id,int hoursDuration,String name) {
-        this.id = id;
-        this.hoursDuration =hoursDuration;
-        this.name = name;
-    }
-
+    @NotNull
     private String descriptionSchedule;
 
     private Day day;
+
+    public Schedule(){
+
+    }
+    public Schedule( int id,int hoursDuration,String name, String descriptionSchedule, Day day) {
+        this.id = id;
+        this.hoursDuration =hoursDuration;
+        this.name = name;
+        this.descriptionSchedule = descriptionSchedule;
+        this.day = day;
+    }
+
+
 
     public String getDescriptionSchedule() {
         return descriptionSchedule;
