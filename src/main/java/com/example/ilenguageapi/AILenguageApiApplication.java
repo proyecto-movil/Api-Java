ccef7a7c16c8d2b5c56bcd9154479d36f5ca28c1
Sebastian <sebaas14toulier@gmail.com>
2021-05-07 23:06:46 -0500

    fix: bug in session controller

--- src/main/java/com/example/ilenguageapi/AILenguageApiApplication.java
+++ src/main/java/com/example/ilenguageapi/AILenguageApiApplication.java
@@ -8,10 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class ILenguageApiApplication {
+public class AILenguageApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ILenguageApiApplication.class, args);
+        SpringApplication.run(AILenguageApiApplication.class, args);
     }
 
     @Bean
