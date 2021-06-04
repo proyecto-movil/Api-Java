ea908dfc463e184b869a345ddc3dda314cf1df71
Sebastian Toulier <sebaas14toulier@gmail.com>
2021-06-03 23:51:25 -0500

    refactor: add query parms

--- src/main/java/com/example/ilenguageapi/ILenguageApiApplication.java
+++ src/main/java/com/example/ilenguageapi/ILenguageApiApplication.java
@@ -1,8 +1,12 @@
 package com.example.ilenguageapi;
 
+import org.apache.catalina.connector.Connector;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -17,4 +21,15 @@ public class ILenguageApiApplication {
     @Bean
     public ModelMapper modelMapper() {return new ModelMapper();}
 
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                connector.setProperty("relaxedQueryChars", "|{}[]");
+            }
+        });
+        return factory;
+    }
 }
--- src/main/java/com/example/ilenguageapi/domain/repository/SubscriptionRepository.java
+++ src/main/java/com/example/ilenguageapi/domain/repository/SubscriptionRepository.java
@@ -3,12 +3,14 @@ package com.example.ilenguageapi.domain.repository;
 
 import com.example.ilenguageapi.domain.model.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
+    @Query("SELECT s FROM Subscription s WHERE s.name = ?1")
     public Optional<Subscription> findByName(String name);
     public Optional<Subscription> findByPrice(int price);
     public Optional<Subscription>findByMonthDuration(int monthDuration);
